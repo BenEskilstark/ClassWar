f67be9618633de696f382918776e7a50c1240ebd
Benjamin Eskildsen <eskildsen.ben@gmail.com>
2022-09-25 16:13:06 -0400

    fix modals and react version

--- js/index.js
+++ js/index.js
@@ -11,21 +11,21 @@ import type {Store} from './types';
 const store = createStore(rootReducer);
 window.store = store; // useful for debugging and a few hacks
 
+
+function renderUI(store: Store): React.Node {
+  const state = store.getState();
+  root.render(
+    <Main dispatch={store.dispatch}
+      store={store} state={state}
+      modal={state.modal}
+    />
+  );
+}
+
 // subscribe the game rendering to the store
+const root = ReactDOM.createRoot(document.getElementById('container'));
 renderUI(store);
 store.subscribe(() => {
   renderUI(store);
 });
-
-
-function renderUI(store: Store): React.Node {
-  const state = store.getState();
-  ReactDOM.render(
-    <Main dispatch={store.dispatch}
-      store={store} state={state}
-      modal={state.modal}
-    />,
-    document.getElementById('container'),
-  );
-}
 
